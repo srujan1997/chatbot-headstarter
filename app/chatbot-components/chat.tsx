df3df8fd95a9656e7d832a93325d642133b15b02
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-08-07 09:22:37 -0400

    new branch creation, will add gemini and basic ui to project.

--- app/chatbot-components/chat.tsx
+++ app/chatbot-components/chat.tsx
@@ -2,6 +2,7 @@ export default function Chatbox() {
     return (
         <>
             <section className="w-full h-full">
+              
                 <div className="w-full h-max flex place-content-end p-5">
                     
                     <div className="rounded-xl px-7 place-items-start place-content-start p-3 bg-green-300 w-max h-max">
@@ -17,6 +18,16 @@ export default function Chatbox() {
                     <div className="rounded-xl px-7 place-items-start place-content-start p-3 bg-blue-300 w-max h-max">
                         <p>
                             Some test chatbot text
+                        </p>
+                    </div>
+
+                </div>
+              
+                <div className="w-full h-max flex place-content-end p-5">
+                    
+                    <div className="rounded-xl px-7 place-items-start place-content-start p-3 bg-green-300 w-max h-max">
+                        <p>
+                            Some test user text
                         </p>
                     </div>
 
