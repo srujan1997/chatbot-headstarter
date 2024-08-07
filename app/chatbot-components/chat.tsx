0f8ae4d1516bf0fdfba5fe56744e82a0e75954d7
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-08-07 09:13:36 -0400

    setting up project, nextjs

---
+++ app/chatbot-components/chat.tsx
@@ -0,0 +1,28 @@
+export default function Chatbox() {
+    return (
+        <>
+            <section className="w-full h-full">
+                <div className="w-full h-max flex place-content-end p-5">
+                    
+                    <div className="rounded-xl px-7 place-items-start place-content-start p-3 bg-green-300 w-max h-max">
+                        <p>
+                            Some test user text
+                        </p>
+                    </div>
+
+                </div>
+              
+                <div className="w-full h-max flex place-content-start p-5">
+                    
+                    <div className="rounded-xl px-7 place-items-start place-content-start p-3 bg-blue-300 w-max h-max">
+                        <p>
+                            Some test chatbot text
+                        </p>
+                    </div>
+
+                </div>
+
+            </section>
+        </>
+    )
+}
