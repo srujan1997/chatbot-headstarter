6e63eaeee326486dd7dda26704f0971a4f782c6e
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-08-09 03:28:52 -0400

    improved ui, loading animation. working on it more

--- app/chatbot-components/chat.tsx
+++ app/chatbot-components/chat.tsx
@@ -1,28 +1,130 @@
 'use client'
 
-import { motion, AnimatePresence } from "framer-motion"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Message, streamConversation } from "../actions";
 import { readStreamableValue } from "ai/rsc";
 import { Input } from "@/components/ui/input"
-import { FaLongArrowAltUp } from "react-icons/fa";
+import { FaLongArrowAltUp, FaSpinner } from "react-icons/fa";
 import rehypeRaw from 'rehype-raw';
 import ReactMarkdown from 'react-markdown'
+import { motion, AnimatePresence, useInView, useAnimation, Variant } from "framer-motion";
+import { useRef } from "react";
+import loadingGif from '../assets/load.gif'
+import { Textarea } from "@/components/ui/textarea";
 
 
+
+type AnimatedTextProps = {
+    text: string | string[];
+    el?: keyof JSX.IntrinsicElements;
+    className?: string;
+    once?: boolean;
+    repeatDelay?: number;
+    animation?: {
+      hidden: Variant;
+      visible: Variant;
+    };
+  };
+
+const defaultAnimations = {
+hidden: {
+    opacity: 0,
+    x: -20,
+},
+visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+    duration: 0.05,
+    },
+},
+  };
+
+export const AnimatedText = ({
+    text,
+    el: Wrapper = "p",
+    className,
+    once,
+    animation = defaultAnimations,
+  }: AnimatedTextProps ) => {
+    const controls = useAnimation();
+    const textArray = Array.isArray(text) ? text : [text];
+    const ref = useRef(null);
+    const isInView = useInView(ref, { amount: 0.25, once });
+  
+    
+    useEffect(() => {
+      let timeout: NodeJS.Timeout;
+      const show = () => {
+        controls.start("visible");
+      };
+  
+      if (isInView) {
+        show();
+      } else {
+        controls.start("hidden");
+      }
+  
+      return () => clearTimeout(timeout);
+    }, [isInView]);
+
+
+  
+    return (
+      <Wrapper className={className}>
+        {/* <span className="sr-only">{textArray.join(" ")}</span> */}
+        <motion.span
+          ref={ref}
+          initial="hidden"
+          animate={controls}
+          variants={{
+            visible: { transition: { staggerChildren: 0.035 } },
+            hidden: {},
+          }}
+          aria-hidden
+        >
+          {textArray.map((line, lineIndex) => (
+            <span className="block" key={`${line}-${lineIndex}`}>
+              {line.split(" ").map((word, wordIndex) => (
+                <span className="inline-block" key={`${word}-${wordIndex}`}>
+                  {word.split("").map((char, charIndex) => (
+                    <motion.span
+                      key={`${char}-${charIndex}`}
+                      className="inline-block"
+                      variants={animation}
+                    >
+                        {/* <ReactMarkdown rehypePlugins={[rehypeRaw]} className="prose-lg">{char}</ReactMarkdown> */}
+                        {char}
+                    </motion.span>
+                  ))}
+                  <span className="inline-block">&nbsp;</span>
+                </span>
+              ))}
+            </span>
+          ))}
+        </motion.span>
+      </Wrapper>
+    );
+  };
 
 export default function Chatbox() {
 
     const [input, setInput] = useState<string>("");
     const [conversation, setConversation] = useState<Message[]>([]);
+    const [thinking, setThinking] = useState<boolean>(false);
+    const chatEndRef = useRef<HTMLDivElement>(null);
+
 
     // gemini function 
     const startChat = async () => {
-        const { messages, newMessage } = await streamConversation([
-            ...conversation,
-            { role: "user", content: input },
-        ]);
 
+      
+      const { messages, newMessage } = await streamConversation([
+        ...conversation,
+        { role: "user", content: input },
+      ]);
+      
+      setThinking(true);
         let textContent = "";
 
         for await (const delta of readStreamableValue(newMessage)) {
@@ -36,16 +138,22 @@ export default function Chatbox() {
 
         console.log(textContent)
         setInput(""); // Clear the input field after submitting
+
+        setThinking(false);
     }
+
+
 
     return (
         <>
             <section className="w-full h-full flex place-content-center" >
                 
+
                 <AnimatePresence>
 
-                <motion.div id="chat-container" className={`w-[calc(70vw-100px)] no-scrollbar h-[calc(100vh-150px)] flex flex-col overflow-y-scroll`}>
+                <motion.div id="chat-container" className={`w-full md:w-[calc(70vw-100px)] no-scrollbar h-[calc(100vh-150px)] flex flex-col overflow-y-scroll py-8`}>
                     
+                    {/* welcome messege */}
                     <motion.div className="w-full h-max flex place-content-start p-5">
                     
                         <motion.div 
@@ -53,12 +161,20 @@ export default function Chatbox() {
                             scale: 1.005 
                         }} 
                         className="cursor-pointer rounded-xl flex gap-6 px-7 place-items-start place-content-start p-3 max-w-[350px] h-max">
-                            <div className="w-10 h-10 rounded-full bg-[#31363F]"></div>
-                            <p>Welcome to our chatbot. Ask us some questions. Our responses will be in blue.</p>
+                            <div className="w-max p-1 h-10 rounded-full bg-[#76ABAE]"></div>
+                            <AnimatedText
+                                el="h2"
+                                text={[
+                                    "Welcome to our chatbot. Ask us some questions. Our responses will be on the left side like this one here.",
+                                ]}
+                                className=""
+                                once
+                            />
                         </motion.div>
 
                     </motion.div>
 
+                    {/* your questions and prompts....  */}
                     <motion.div className="w-full h-max flex place-content-end p-5">
                     
                         <motion.div 
@@ -66,28 +182,41 @@ export default function Chatbox() {
                             scale: 1.005 
                         }}
                         className="cursor-pointer rounded-xl px-7 place-items-start place-content-start p-3 bg-[#31363F] max-w-[350px] h-max">
-                            <p>Your questions and prompts will be in green here.</p>
+                            <AnimatedText
+                                el="h2"
+                                text={[
+                                    "Your questions and prompts will be on the right side like this one here.",
+                                ]}
+                                className=""
+                                once
+                            />
                         </motion.div>
 
                     </motion.div>
 
-                        <motion.div
-                        className="w-full h-[1px] flex bg-[#31363F] my-8"
-                        />
-                    
-                    {conversation.map((message) => (
+                    {/* divider  */}
+                    {/* <motion.div
+                    className="w-full h-[1px] flex bg-[#76ABAE] my-8"
+                    /> */}
+
+
+                    {/* conversation */}
+                    {conversation.map((message, index) => (
                         <motion.div key={message.content} className={`w-full h-max flex p-5 ${message.role === 'user' ? 'place-content-end' : 'place-content-start'}`} >
+                    
                             <motion.div 
-                            whileHover={{ 
-                                scale: 1.005 
-                            }}
+                          
                             className={`cursor-pointer flex gap-6 rounded-xl px-7 place-items-start place-content-start p-3 sm:max-w-[500px] h-max  ${message.role === 'user' ? 'bg-[#31363F]' : 'bg-transparent'}`} key={message.content}>
-                                {message.role !== 'user' && <div className="w-10 h-10 rounded-full bg-[#31363F]"></div>}
-                                <ReactMarkdown rehypePlugins={[rehypeRaw]} className="prose-lg">{message.content}</ReactMarkdown>
+                                  {message.role != 'user' && <div className="w-max p-1 h-10 rounded-full bg-[#76ABAE]"></div>}
+                                  <ReactMarkdown rehypePlugins={[rehypeRaw]} className="prose-lg">{message.content}</ReactMarkdown>
+
+                                {/* {message.content} */}
                             </motion.div>
+
                         </motion.div>
                     ))}
-
+                  
+                    <div className="w-full h-max bg-[#76ABAE] " ref={chatEndRef} />
 
                 </motion.div>
                             
@@ -97,19 +226,27 @@ export default function Chatbox() {
                 <div className="absolute  bottom-0 place-self-center p-4 flex place-self-end justify-between  gap-8 place-items-center w-[90%] h-max">
                
                     <div className="w-full">
-                        <Input 
+                        <Textarea 
                             value={input}
                             onChange={(event) => {
                                 setInput(event.target.value);
                             }} 
-                            className="outline outline-transparent border-transparent bg-[#31363F]" autoFocus />
+                            className="outline no-scrollbar outline-transparent border-transparent bg-[#31363F]" autoFocus />
                     </div>
 
-                    <button className="bg-[#31363F] p-3 rounded-full"
-                        onClick={() => startChat()}
-                    >
-                        <FaLongArrowAltUp size={20} />
-                    </button>
+                        <motion.div
+                        >
+                            <button className="bg-[#76ABAE] p-3 rounded-full"
+                                onClick={() => startChat()}
+                            >
+                              {thinking ? (
+                                <FaSpinner size={20} className="animate-spin" />
+                                ) : (
+                                  <FaLongArrowAltUp size={20} />
+                                )}
+                            </button>
+                        </motion.div>
+                    
 
                 </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import Chatbox from "./chatbot-components/chat";
 
 export default function Home() {
   return (
-    <main className="bg-[#222831] text-white flex min-h-screen flex-col gap-2 p-10">
-      <div className="text-xs w-full h-max flex justify-evenly">
+    <main className="bg-[#222831] text-white overflow-hidden flex min-h-screen flex-col gap-2 p-5 md:p-10">
+      
+      <div className="text-xs w-full h-max flex justify-between">
         <h1>Chatbot Headstarter</h1>
-        <h1>stuff</h1>
+        <h1></h1>
       </div>
 
       <Chatbox />
