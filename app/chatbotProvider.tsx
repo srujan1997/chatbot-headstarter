a1f003e4df14c1de7c917142811bdd732719a517
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-08-10 05:41:10 -0400

    chat updated :D

---
+++ app/chatbotProvider.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React, { createContext, useState, useContext } from 'react';
+
+interface ChatbotContextType {
+    isThinking: boolean;
+    setIsThinking: (value: boolean) => void;
+}
+
+const ChatbotContext = createContext<ChatbotContextType | null>(null);
+
+export const ChatbotProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isThinking, setIsThinking] = useState(false);
+
+  return (
+    <ChatbotContext.Provider value={{
+        isThinking,
+        setIsThinking
+        }}>
+      {children}
+    </ChatbotContext.Provider>
+  );
+};
+
+export const useChatbot = () => {
+  const context = useContext(ChatbotContext);
+  if (!context) throw new Error('useChatbot must be used within a ChatbotProvider');
+  return context;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
+'use client'
+
 import Image from "next/image";
 import Chatbox from "./chatbot-components/chat";
+import { FaSpinner } from "react-icons/fa";
+import { useChatbot } from "./chatbotProvider";
+import { motion } from "framer-motion";
 
 export default function Home() {
+
+const { isThinking, setIsThinking } = useChatbot();
+
   return (
-    <main className="bg-[#222831] text-white overflow-hidden flex min-h-screen flex-col gap-2 p-5 md:p-10">
+    <main className="bg-[#222831] relative text-white overflow-hidden flex min-h-screen flex-col gap-2 p-5 md:p-10">
       
+      {isThinking && (
+        <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }} 
+        className="absolute top-10 right-10"> <FaSpinner size={25} className="animate-spin text-[#76ABAE]" /></motion.div>
+      )}
+
+
       <div className="text-xs w-full h-max flex justify-between">
         <h1>Chatbot Headstarter</h1>
         <h1></h1>
